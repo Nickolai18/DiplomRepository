24744413c6cfa5560029d8790e544369da395125
Nickolai18 <nick.rotari.42@gmail.com>
2026-03-10 15:21:39 +0300

    2 попытка

--- python/main.py
+++ python/main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 def index():
     return {"message": "Привет, МИР!"}
 
+@app.get("/items/{item_id}")
+def task(item_id: int):
+    return {"item_id": item_id, "name": f"Задача номер {item_id}"}
